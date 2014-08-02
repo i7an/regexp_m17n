e096cbe7e7bf1dedec1fdfd960d79e5c0511598d
ivan <vanya.yu@gmail.com>
2014-08-02 23:15:38 +0300

    done!

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,17 @@
 module RegexpM17N
   def self.non_empty?(str)
+    unless @is_string_monkey_patched
+      String.class_eval do
+        alias_method :old_encode, :encode
+        def encode(enc)
+          old_encode
+        rescue Encoding::UndefinedConversionError
+          self
+        end
+      end
+      @is_string_monkey_patched = true
+    end
+    
     str =~ /^.+$/
   end
 end
